6d9a79fee5bba0ed9cd965b2b0e5a74b0aa6df8b
Asmitshadow <rishistudent77@gmail.com>
2025-11-28 22:55:05 +0530

    assignment

---
+++ task1.py
@@ -0,0 +1,66 @@
+#Task 1: Perform Basic Mathematical Operations
+#Problem Statement: Write a Python program that does the following:
+#1.  Takes two numbers as input from the user.
+#2.  Performs the basic mathematical operations on these two numbers:
+#o	Addition
+#o	Subtraction
+#o	Multiplication
+#o	Division
+#3.  Displays the results of each operation on the screen.
+
+
+first_number =  80
+
+second_number =  16 
+
+a = first_number+second_number
+b = first_number-second_number
+c= first_number*second_number
+d= first_number/second_number
+
+print("first_number =", 80 , " and second_number =", 16)
+print("addition =", a)
+print("subtraction =",b)
+print("Multiplication = " , c)
+print("Division =" ,float(d))
+
+
+           # by input tag
+
+# addition
+
+x =int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+result = x + y
+print("Addition =", result)
+
+
+#Substraction
+
+x1 =  int(input("Enter first number:  "))
+y1 =  int(input("Enter second number: "))
+
+result1 = x1 - y1
+print("substraction =", result1)
+
+# Multiplication
+
+x2 =  int(input("Enter first number:  "))
+y2 =  int(input("Enter second number: "))
+
+result2 = x2 * y2
+print("multiplication =", result2)
+
+
+#Divison
+
+
+x3 =  int(input("Enter first number:  "))
+y3 =  int(input("Enter second number: "))
+
+result3 = x3 / y3
+print("division =", result)
+
+
+
